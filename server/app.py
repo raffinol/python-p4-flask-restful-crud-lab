d354863d32c7a27a86c4a73f53cd9ca3fe1bdf9a
Rafael Finol <rafaelfinol88@gmail.com>
2023-09-15 20:08:10 -0400

    lab complete

--- server/app.py
+++ server/app.py
@@ -7,8 +7,8 @@ from flask_restful import Api, Resource
 from models import db, Plant
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///plants.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
@@ -18,7 +18,6 @@ api = Api(app)
 
 
 class Plants(Resource):
-
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(jsonify(plants), 200)
@@ -27,9 +26,9 @@ class Plants(Resource):
         data = request.get_json()
 
         new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
+            name=data["name"],
+            image=data["image"],
+            price=data["price"],
         )
 
         db.session.add(new_plant)
@@ -38,18 +37,42 @@ class Plants(Resource):
         return make_response(new_plant.to_dict(), 201)
 
 
-api.add_resource(Plants, '/plants')
+api.add_resource(Plants, "/plants")
 
 
 class PlantByID(Resource):
-
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        data = request.get_json()
 
-api.add_resource(PlantByID, '/plants/<int:id>')
+        plant = Plant.query.filter(Plant.id == id).first()
+        for attr in data:
+            setattr(plant, attr, data[attr])
+
+        db.session.add(plant)
+        db.session.commit()
+
+        response_dict = plant.to_dict()
+
+        response = make_response(response_dict, 200)
+
+        return response
+
+    def delete(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        response = make_response("", 204)
+        return response
 
 
-if __name__ == '__main__':
+api.add_resource(PlantByID, "/plants/<int:id>")
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
